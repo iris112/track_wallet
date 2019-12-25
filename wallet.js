e57752984f7e55fd499adfcf16e9025e155b012d
iris112 <nglayi99@gmail.com>
2019-12-25 14:23:39 -0500

    add track wallet function with storing db

--- wallet.js
+++ wallet.js
@@ -1,1 +1,4 @@
-module.exports = ["0x0FE1829403d422470cd4cf0aBAd4bCEc9aA2eBF6".toLowerCase()]
+module.exports = [
+"0x0FE1829403d422470cd4cf0aBAd4bCEc9aA2eBF6".toLowerCase(),
+"0x4d9c2CAda346DaBf2d200AA9E2597d8e8Ec4015b".toLowerCase()
+]
